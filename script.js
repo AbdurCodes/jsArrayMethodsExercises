47dba521e2f246fcee0370ba01c1a5f0b18485a5
AbdurCodes <124054847+AbdurCodes@users.noreply.github.com>
2024-04-11 01:15:15 +0500

    filter items in array modifying the array in place

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Q No. 1
+
 // Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”
 
 // my solution
@@ -31,3 +33,75 @@ console.log(camelize('-webkit-transition'));
 //       )
 //       .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
 //   }
+
+
+
+
+
+
+
+// Q No. 2
+
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+// The function should not modify the array. It should return the new array.
+
+
+function filterRange(arr, a, b){
+    // let newArray = arr.filter(value => value >= a && value <= b);
+    let newArray = arr.filter(value => a <= value && b >= value);
+    return newArray;
+}
+
+let arr = [5, 3, 4, 2, 5, 6, 8, 1];
+
+console.log(filterRange(arr, 1, 4));
+console.log(arr);
+
+
+
+
+
+
+
+
+
+// Q No. 3
+
+// Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
+// The function should only modify the array. It should not return anything.
+
+
+
+// solution 1: my effort + gemini magic
+function filterRangeInPlace(arr, a, b){
+    let indexToWrite = 0;
+    for (let i=0; i<arr.length; i++){
+        if (arr[i] >= a && arr[i] <= b) {
+            arr[indexToWrite] = arr[i];
+            indexToWrite++;
+        }
+    }
+    arr.length = indexToWrite;
+    return arr;
+}
+
+let arr123 = [5, 3, 8, 1];
+console.log(filterRangeInPlace(arr123, 1, 4));
+console.log(arr123);
+
+
+
+// solution 2: as solved by https://javascript.info/array-methods
+function filter_Range_In_Place(arr, a, b) {
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] < a || arr[i] > b) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
+}
+
+let arr1234 = [5, 3, 8, 1];
+console.log(filter_Range_In_Place(arr1234, 1, 4));
+console.log(arr1234);
