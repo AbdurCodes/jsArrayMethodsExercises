dd83752a287572e7dd912a2d7b9681bdc918de7f
AbdurCodes <124054847+AbdurCodes@users.noreply.github.com>
2024-04-11 17:39:16 +0500

    Map to objects js exercise

--- script.js
+++ script.js
@@ -165,16 +165,45 @@ console.log(arrToSort);
 
 
 
+
+
+
 // Q No. 6
 // Map to names
 // You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
 
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [ john, pete, mary ];
+
+// my solution
+// let names = users.map(user => user.name);
+// console.log(names);
+
+
+
+
+
+
+
+// Q No. 7
+// Map to objects
+// You have an array of user objects, each one has name, surname and id.
+// Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
 
 let users = [ john, pete, mary ];
 
-// my solution
-let names = users.map(user => user.name);
-console.log(names);
+// to return array of objects we need to wrap our curly braces in parenthesis, ({})
+let usersMapped = users.map(user => ({
+    'fullName' : `${user.name} ${user.surname}`,
+    'id' : user.id
+}))
+
+console.log(usersMapped);
+console.log(users);
